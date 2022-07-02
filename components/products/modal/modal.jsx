413f3256b46366c52992acc98bce54629dc33ee0
Chiara <chiara-zuc@libero.it>
2022-07-02 16:17:34 +0200

    fix: accento

--- components/products/modal/modal.jsx
+++ components/products/modal/modal.jsx
@@ -7,12 +7,12 @@ export const Modal = ({ message }) => {
         <h4>{message}</h4>
         {message.includes("carrello") ? (
           <p>
-            Puoi vedere il riepilogo cliccando nell&apos icona del carrello che
+            Puoi vedere il riepilogo cliccando nell &apos icona del carrello che
             si trova in alto 😉👍
           </p>
         ) : (
           <p>
-            Puoi vedere i tuoi prodotti preferiti cliccando nell&apos icona del
+            Puoi vedere i tuoi prodotti preferiti cliccando nell &apos icona del
             cuore in alto 😉👍
           </p>
         )}
